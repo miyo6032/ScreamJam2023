6228e435938f65a247d5ffee891060392c2a8e27
Barribob Development <barribobdevelopment@gmail.com>
2023-10-21 17:34:07 -0600

    Start capture logic

---
+++ Scripts/ChaseHandler.gd
@@ -0,0 +1,9 @@
+extends Path3D
+
+@export var gumkid: Gumkid
+@export var path_follow: PathFollow3D
+
+func _ready():
+    var tween = get_tree().create_tween()
+    gumkid.set_speed(1)
+    tween.tween_property(path_follow, "progress_ratio", 1, 25)
---
+++ Scripts/Gumkid.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+class_name Gumkid
+
+@onready var model := $GumkidModel
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    pass
+
+
+func _on_capture_area_body_entered(body):
+    print("captured!")
+
+
+func set_speed(speed):
+    model.set_speed(speed)
