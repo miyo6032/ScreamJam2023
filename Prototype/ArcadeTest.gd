8ec303dbc039a34e23a0a54c2f0a60fb60f32bfe
Barribob Development <barribobdevelopment@gmail.com>
2023-10-23 08:34:10 -0600

    trying to get subviewport working on export

--- Prototype/ArcadeTest.gd
+++ Prototype/ArcadeTest.gd
@@ -1,5 +1,10 @@
 extends MeshInstance3D
 
+@export var sprite: Sprite3D
+
 func _ready():
-    var viewport = $SubViewport
-    $Screen.get_surface_override_material(0).albedo_texture = viewport.get_texture()
+    pass
+#    var viewport = $SubViewport
+#    $Screen.get_surface_override_material(0).albedo_texture = viewport.get_texture()
+
+#    sprite.texture = viewport.get_texture()
