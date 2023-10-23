59b2366b05c381a2294c30466eb4e2ecd9782476
Barribob Development <barribobdevelopment@gmail.com>
2023-10-22 19:27:59 -0600

    Add texture to 2D game

--- Prototype/Scenes/ArcadeGame.gd
+++ Prototype/Scenes/ArcadeGame.gd
@@ -7,6 +7,8 @@ signal game_crash
 @onready var path_follow := $Path2D/PathFollow2D
 @onready var starting_screen = $Camera2D/UserInterface/StartingScreen
 
+@export var camera_target: Marker2D
+
 var player_died = false
 var enabled = true
 
@@ -21,9 +23,9 @@ func _physics_process(delta):
             starting_screen.hide()
             player.enabled = true
             var tween = get_tree().create_tween()
-            tween.tween_property(path_follow, "progress_ratio", 1, 10.0)
+            tween.tween_property(path_follow, "progress_ratio", 1, 13.0).set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_SINE)
             var tween2 = get_tree().create_tween()
-            tween2.tween_callback(_crash_after_delay).set_delay(8)
+            tween2.tween_callback(_crash_after_delay).set_delay(9)
 
 func _crash_after_delay():
     if enabled:
@@ -33,7 +35,7 @@ func _crash_after_delay():
 func _process(delta):
     if !enabled:
         return
-    camera.position = player.position
+    camera.global_position = camera_target.global_position
 
 func _on_game_crash():
     $Camera2D/UserInterface/CrashScreen.show()
--- Scripts/ArcadePlayerMovement.gd
+++ Scripts/ArcadePlayerMovement.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 100.0
 
 var enabled = false
 
