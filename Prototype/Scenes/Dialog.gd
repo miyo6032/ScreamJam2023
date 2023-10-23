b0a28f925df3036f1200914d4354eb848a5865a5
Barribob Development <barribobdevelopment@gmail.com>
2023-10-22 23:23:39 -0600

    make dialog close after a few seconds

--- Prototype/Scenes/Dialog.gd
+++ Prototype/Scenes/Dialog.gd
@@ -2,13 +2,20 @@ extends Label
 
 class_name Dialog
 
-var pc: float
+var tween
 
 func _ready():
     visible_ratio = 0.0    
     
 func show_dialog(dialog, time):
+    if tween:
+        tween.kill()
+
     visible_ratio = 0.0    
     text = dialog
-    var tween = get_tree().create_tween()
+    tween = get_tree().create_tween()
     tween.tween_property(self, "visible_ratio", 1.0, time)
+    tween.tween_callback(reset).set_delay(5.0)
+
+func reset():
+    visible_ratio = 0.0
